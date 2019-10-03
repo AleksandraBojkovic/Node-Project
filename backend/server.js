b3ddeced2f4f6e79085fdd3799e9575b03ea2baf
Aleksandra Bojkovic <43954183+AleksandraBojkovic@users.noreply.github.com>
2019-10-03 09:50:40 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,25 +9,12 @@ const config = require('config');
 const app = express();
 const DIR = './routes/uploads';
 
-// mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
-//   console.log("Connected to DB!?")
-// );
-
-const env = process.env.NODE_ENV || 'development';
-
-if(env === 'test'){
-  process.env.MONGODB_URI = process.env.TEST_DB;
-} else {
-  process.env.MONGODB_URI = process.env.DB_CONNECTION;
-}
-mongoose.connect(process.env.MONGODB_URI,  { useNewUrlParser: true }, () => 
-  console.log(process.env.MONGODB_URI)
-  );
-
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+  console.log("Connected to DB!?")
+);
 
 app.use(bodyParser.json());
 
-// CORS 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Credentials', true);
   res.header("Access-Control-Allow-Origin", "http://localhost:4200");
@@ -37,19 +24,6 @@ app.use(function(req, res, next) {
 });
 
 app.use("/issues", issuesRoutes);
-
-app.use(multer({
-  dest: DIR,
-  rename: function (fieldname, filename) {
-    return filename + Date.now();
-  },
-  onFileUploadStart: function (file) {
-    console.log(file.originalname + ' is starting ...');
-  },
-  onFileUploadComplete: function (file) {
-    console.log(file.fieldname + ' uploaded to  ' + file.path);
-  }
-}).single('file'));
 
 app.listen(9001);
 
